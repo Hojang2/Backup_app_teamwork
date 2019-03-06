9a843af4229a3c8d8c0ffde938885f3024a99ec8
Jan Buchmaier <janbuchmaier@seznam.cz>
2019-03-06 13:16:46 +0100

    Added path option, validating if directory with this path exist and if there is no path argument sets default path specific for current platform

--- app/main.py
+++ app/main.py
@@ -2,16 +2,36 @@
 # -*- encoding: utf-8 -*-
 
 import argparse
+import sys
+import os
 
 parser = argparse.ArgumentParser(description="Backup client application",
                                  formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-g' '--graphics', dest='graphics', action='store_true')
-parser.add_argument('--no-graphics', dest='graphics', action='store_false')
+parser.add_argument('-g' '--graphics', dest='graphics', action='store_true',
+                    help='Enables GUI')
+parser.add_argument('--no-graphics', dest='graphics', action='store_false',
+                    help='Disables GUI')
 parser.set_defaults(graphics=False)
+parser.add_argument('-p' '--path', action='store',
+                    help='Path to directory for backup')
 args = parser.parse_args()
 
 
 def main():
+    kwargs = {}
+
+    if args.path:
+        if os.path.isdir(args.path):
+            kwargs["path"] = args.path
+        else:
+            raise NotADirectoryError("Target path is not directory")
+
+    else:
+        if sys.platform == "linux":
+            kwargs["path"] = "/home/"
+        elif sys.platform.startswith('win'):
+            kwargs["path"] = "C:"
+
     if args.graphics:
         pass
         # Starts graphics session.
