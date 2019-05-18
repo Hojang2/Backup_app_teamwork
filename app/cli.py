e02965a1d98a25d41609840b3fa39514248a4038
Jan Buchmaier <janbuchmaier@seznam.cz>
2019-05-18 19:54:46 +0200

    Added basic restore version ready for testing

--- app/cli.py
+++ app/cli.py
@@ -1,38 +1,129 @@
 # -*- encoding: utf-8 -*-
+
+"""
+Module with Client
+"""
+
 import os
 import time
 
 
 class Client:
+    """
+    Client class
+    contain methods:
+        backup
+        get_tree
+        restore
+    """
 
-    def __init__(self, platform, path, output):
-
+    def __init__(self, platform, path, output, rest):
+        """
+        Class Initialization
+        :param platform:
+        :param path:
+        :param output:
+        :param restore:
+        """
         self.platform = platform
         self.path = path
         self.output = output
+        self.rest = rest
         self.files = []
+        self.paths = []
+        self.dirs = []
 
     def backup(self):
+        """
+        Method for backing up directory
+        from path to file, that will be stored
+        in output directory
+        :return:
+        """
         self.get_tree()
-        t = time.clock()
+        print(self.files)
         name = "{}Backup_{}.backup".format(self.output, time.ctime())
-        name = name.replace(" ", "_")
-        with open(name, "bw") as f:
-            for file in self.files:
+        name = name.replace(" ", "_").replace(":", "_")
+        with open(name, "bw") as output_file:
+            for directory in self.dirs:
+                output_file.write(directory.encode() + b"\n")
+            for i in range(len(self.files)):
                 try:
-                    ff = open(file, "rb")
-                    print('Backing up ' + file)
-                    f.write(ff.read())
-                    ff.close()
-                except FileNotFoundError as e:
-                    print("File {} wasn't found".format(file))
-                except OSError as e:
-                    print("File {} wasn't found".format(file))
+                    tmp = open(self.files[i], "rb")
+                    print('Backing up ' + self.files[i])
+                    output_file.write(b"******************************************\n")
+                    output_file.write(self.files[i].replace(self.path, "").encode() + b"\n")
+                    output_file.write(b"******************************************\n")
+                    output_file.write(tmp.read())
+                    tmp.close()
+                except FileNotFoundError as error:
+
+                    print(error)
+                    print("File {} wasn't found".format(self.files[i]))
+
+                except OSError as error:
+
+                    print(error)
+                    print("File {} wasn't found".format(self.files[i]))
+
 
         print("Backup finished in {}".format(time.clock()))
 
     def get_tree(self):
+        """
+        Method for getting all directories and
+        subdirectories as self.files and self.paths
+        :return:
+        """
 
         for path, dirs, files in os.walk(self.path):
+
+            for dir in dirs:
+                if dir not in self.dirs:
+                    dir = (path + "/" + dir).replace(self.path, "")
+                    if dir[0] == "/":
+                        dir = dir[1:]
+                    self.dirs.append(dir)
+
             for file in files:
-                self.files.append(path + "/" + file)
+                if file not in self.files:
+                    self.files.append((path + file))
+
+    def restore(self):
+        """
+        Method for restoring backup.
+        Work as reversed backup method and
+        takes the same arguments
+        as path and output,
+        but path is now path to the file, not directory
+        :return:
+        """
+        with open(self.path, "rb") as file:
+            backup = file.read()
+        self.split_backup(backup)
+        print(self.dirs)
+        for i in range(len(self.dirs)):
+            try:
+                os.mkdir(self.output + self.dirs[i].decode("utf8"))
+
+            except FileExistsError as e:
+                print(e)
+        for i in range(len(self.paths)):
+            with open(self.output.encode() + self.paths[i], "bw") as f:
+                f.write(self.files[i])
+
+    def split_backup(self, backup):
+        backup = backup.split(b"******************************************\n")
+        self.dirs = []
+        backup[0] = backup[0].split(b"\n")[:-1]
+        for i in backup[0]:
+            self.dirs.append(i)
+
+        for i in range(1, (len(backup)//2)+1, 2):
+            self.paths.append(backup[i].replace(b"\n", b""))
+            self.files.append(backup[i + 1])
+
+
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-import argparse
+from argparse import RawTextHelpFormatter, \
+    Namespace, ArgumentParser
 import sys
 import os
-
 from cli import Client
 
 
 def main():
-    kwargs = {}
     platform = sys.platform
-    kwargs["platform"] = platform
+    kwargs = {"platform": platform}
+    if arguments.restore:
+        kwargs["restore"] = True
+    else:
+        kwargs["restore"] = False
 
-    if args.path:
-        if os.path.isdir(args.path):
-            kwargs["path"] = args.path
-        else:
-            raise NotADirectoryError("Target path is not directory")
+    if arguments.path:
+
+        kwargs["path"] = arguments.path
+
 
     else:
         if platform == "linux":
@@ -25,40 +27,46 @@ def main():
         elif "win" in platform:
             kwargs["path"] = "C:"
     print(kwargs["path"])
-    if args.output:
-        if os.path.isdir(args.output):
-            kwargs["output"] = args.output
+    if arguments.output:
+        if os.path.isdir(arguments.output):
+            kwargs["output"] = arguments.output
         else:
             raise NotADirectoryError("Target output path is not directory")
     else:
         kwargs["output"] = ""
 
-    if args.graphics:
+    if arguments.graphics:
         pass
         # Starts graphics session.
     else:
-        client = Client(kwargs["platform"], kwargs["path"], kwargs["output"])
-        client.backup()
-        # Runs only CLI
+        client = Client(kwargs["platform"], kwargs["path"], kwargs["output"],
+                        kwargs["restore"])
+        print(client.rest)
+        if client.rest:
+            client.restore()
+        else:
+            client.get_tree()
+            client.backup()
 
 
-parser = argparse.ArgumentParser(description="Backup client application",
-                                 formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-g' '--graphics', dest='graphics', action='store_true',
-                    help='Enables GUI')
-parser.add_argument('--no-graphics', dest='graphics', action='store_false',
-                    help='Disables GUI')
-parser.set_defaults(graphics=False)
-parser.add_argument('-p', '--path', action='store',
-                    help='Path to directory for backup')
-parser.add_argument('-o', '--output', action='store',
-                    help='Path where the backup will be stored')
-parser.add_argument('-r', '--restore', action='store_true',
-                    help='Restores backup from target path to output')
-args = parser.parse_args()
-
+args_parser = ArgumentParser(description="Backup client application",
+                             formatter_class=RawTextHelpFormatter)
+args_parser.add_argument('-g' '--graphics', dest='graphics', action='store_true',
+                         help='Enables GUI')
+args_parser.add_argument('--no-graphics', dest='graphics', action='store_false',
+                         help='Disables GUI')
+args_parser.set_defaults(graphics=False)
+args_parser.add_argument('-p', '--path', action='store',
+                         help='Path to directory for backup')
+args_parser.add_argument('-o', '--output', action='store',
+                         help='Path where the backup will be stored')
+args_parser.add_argument('-r', '--restore', action='store_true',
+                         help='Restores backup from target path to output',
+                         default=False)
+arguments: Namespace = args_parser.parse_args()
+print(type(arguments))
 if __name__ == "__main__":
     try:
         main()
-    except KeyboardInterrupt as e:
-        pass
+    except KeyboardInterrupt as error:
+        print(error)
